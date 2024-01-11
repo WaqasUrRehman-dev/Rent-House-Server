7c4fdff58b1659742a62a9f4f2578fcde2353c91
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-01-11 11:25:30 -0800

    FEAT: role by default admin

--- Users/schema.js
+++ Users/schema.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true },
+    role: { type: String, default: "admin" },
     address: { type: String, required: true },
     city: { type: String, required: true },
     joining: { type: Date, default: Date.now },
