c5cfc9e7eaa813f84e3322dcb2da8c5fb43582d9
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-02-29 13:49:49 +0500

    update

--- Users/schema.js
+++ Users/schema.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-<<<<<<< HEAD
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -15,17 +14,6 @@ const userSchema = new Schema({
     default: "https://cdn-icons-png.flaticon.com/512/7605/7605078.png",
   },
 });
-=======
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: "user" },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    joining: { type: Date, default: Date.now },
-    profile_pic: { type: String, default: "https://cdn-icons-png.flaticon.com/512/7605/7605078.png" }
-})
->>>>>>> 4413dccd8d784cda79b89ec2be32869f7bf46392
 
 const Users = model("user", userSchema);
 module.exports = Users;
